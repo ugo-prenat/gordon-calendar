8e13d32a960d9da1e064d40b93081c1480f19a57
Ugo PRENAT <ugo.prenat@edu.itescia.fr>
2024-04-14 10:14:51 +0200

    feat: init nav quit settings

--- apps/front/src/components/nav/settings/SettingsHoverCard.tsx
+++ apps/front/src/components/nav/settings/SettingsHoverCard.tsx
@@ -1,7 +1,40 @@
+import { Button } from '@src/components/ui/shadcn/button';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuPortal,
+  DropdownMenuSub,
+  DropdownMenuSubContent,
+  DropdownMenuSubTrigger,
+  DropdownMenuTrigger
+} from '@src/components/ui/shadcn/dropdown-menu';
 import { SlidersHorizontal } from 'lucide-react';
 
 const SettingsHoverCard = () => {
-  return <SlidersHorizontal className="w-5 h-5" />;
+  return (
+    <>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button variant="ghost" size="icon">
+            <SlidersHorizontal className="w-4 h-4" />
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent className="w-56">
+          <DropdownMenuSub>
+            <DropdownMenuSubTrigger>theme</DropdownMenuSubTrigger>
+            <DropdownMenuPortal>
+              <DropdownMenuSubContent>
+                <DropdownMenuItem>dark</DropdownMenuItem>
+                <DropdownMenuItem>light</DropdownMenuItem>
+                <DropdownMenuItem>system</DropdownMenuItem>
+              </DropdownMenuSubContent>
+            </DropdownMenuPortal>
+          </DropdownMenuSub>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </>
+  );
 };
 
 export default SettingsHoverCard;
